7c3c7979e791a04d341ec8d2cbdb65ec7eb339b6
okasaka <okasaka@rightcode.co.jp>
2021-09-26 16:48:26 +0900

    add circle spinner

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Sample page!</h1>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
---
+++ src/components/CircleSpinner.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+type Props = { loading: boolean; size?: number; color?: string };
+
+export const CircleSpinner: React.FC<Props> = ({
+  loading,
+  size = 60,
+  color = "#00ced1",
+}) => {
+  return (
+    <>
+      {loading && (
+        <div
+          className="rotate-infinite"
+          style={{
+            display: "inline-block",
+            position: "relative",
+            height: `${size}px`,
+            width: `${size}px`,
+            boxSizing: "border-box",
+          }}
+        >
+          <div
+            style={{
+              display: "block",
+              position: "relative",
+              height: `${size}px`,
+              width: `${size}px`,
+              color: `${color}`,
+              borderRadius: "50%",
+              border: `8px solid ${color}`,
+              borderColor: "transparent",
+              borderTopColor: `${color}`,
+              boxSizing: "border-box",
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: `${size}px`,
+              height: `${size}px`,
+              borderRadius: "50%",
+              border: `1px solid ${color}`,
+              boxSizing: "border-box",
+            }}
+          ></div>
+        </div>
+      )}
+    </>
+  );
+};
